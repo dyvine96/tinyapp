14f8b9db1d38db974663d631bd625850d1732b43
Dymika Harte <dymika@unsgnd.ca>
2021-12-07 01:39:22 +0000

    added body parser

--- express_server.js
+++ express_server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
-app.get("urls/new", (req, res) => {
-  res.render("urls_new");
-});
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,6 +16,10 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+}); 
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
