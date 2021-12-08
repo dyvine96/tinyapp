657f6efd137fac316dd38cb10309c5f7ae3ce4b2
Dymika Harte <dymika@unsgnd.ca>
2021-12-08 23:11:50 +0000

    removed typo

--- express_server.js
+++ express_server.js
@@ -46,7 +46,7 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {a
+app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
